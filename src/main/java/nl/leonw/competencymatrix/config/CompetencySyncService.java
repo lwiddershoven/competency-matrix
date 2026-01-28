ed844d17ab59ae0bb358f594a801cf5187e26128
Leon <git@qaz.dds.nl>
2026-01-28 13:08:28 +0100

    Optimeze imports

--- src/main/java/nl/leonw/competencymatrix/config/CompetencySyncService.java
+++ src/main/java/nl/leonw/competencymatrix/config/CompetencySyncService.java
@@ -18,11 +18,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.Yaml;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
